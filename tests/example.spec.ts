ef6c2eb49dfa8d91c6316eef52a7287253286bea
Hoang <vhoang1928@gmail.com>
2026-04-06 22:38:58 +0700

    update baitap 1- Buoi 1

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,20 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://material.playwrightvn.com/');
 
-  // Expect a title "to contain" a substring.
+ //Buoi 1 
+ //Bai 1
+  // Expect a title "Playwright" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://material.playwrightvn.com/');
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // Click the Bài học 1: Register Page.
+  await page.getByRole('link', { name: 'Bài học 1: Register Page' }).click();
 
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  // Expects page to have a heading with the name of User Registration.
+  await expect(page.getByRole('heading', { name: 'User Registration' })).toBeVisible();
 });
